03b198d5bb9ecb105da6be40ab33c6c99ac399bd
DatNguyen1091 <nvdat0204@gmail.com>
2023-03-12 22:07:16 +0700

    Sort array with POST method

---
+++ MinimalNetCore6/MinimalNetCore6/MapEndpoints.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalNetCore6
+{
+    public static class MapEndpoints
+    {
+        public static WebApplication MapArray(this WebApplication app)
+        {
+            app.MapPost("Sort/", ([FromBody] ArrayModel model) =>
+            {
+                var sortedNumber = model.ArrayNumber!.ToArray();
+                var id = model.SortType;
+                if (id == 0)
+                {
+                    Array.Sort(sortedNumber);
+                }
+                else if (id == 1)
+                {
+                    for (int i = 0; i < sortedNumber.Length; i++)
+                    {
+                        for (int j = i + 1; j < sortedNumber.Length; j++)
+                        {
+                            if (sortedNumber[i] < sortedNumber[j])
+                            {
+                                int temp = sortedNumber[i];
+                                sortedNumber[i] = sortedNumber[j];
+                                sortedNumber[j] = temp;
+                            }
+                        }
+                    }
+                }
+                return sortedNumber;
+            });
+            return app;
+        }
+    }
+}
